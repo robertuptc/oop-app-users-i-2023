4eaab7a345fa20bb75253448897e28fc2b679323
robertuptc <robertuptc@gmail.com>
2023-11-08 22:44:15 -0500

    class User created

--- User.py
+++ User.py
@@ -1,1 +1,13 @@
-# your User class goes here
+class User:
+    def __init__(self, name, last_name, age, email, password ):  
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+        self.password = password
+
+    def __str__(self):
+        return f"Welcome {self.name}! Your account has been created. Check your {self.email} for a confirmation code!"
+
+new_user = User('Robert', 'Puentes', 25, "myemail.@gmail.com", "123asd456")
+print(new_user.name)
